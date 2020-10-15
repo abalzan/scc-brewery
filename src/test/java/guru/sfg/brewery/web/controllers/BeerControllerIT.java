fd5f1865c4a6aeef9515c02f97a6e9c1b945de42
Andrei Balzan <andreibalzan@gmail.com>
2020-10-15 15:47:39 +0100

    configure in memory user

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -26,6 +26,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class BeerControllerIT extends BaseIT {
 
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(SecurityMockMvcRequestPostProcessors.httpBasic("user", "passwd")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
     @WithMockUser("spring")
     @Test
     void findBeers() throws Exception {
