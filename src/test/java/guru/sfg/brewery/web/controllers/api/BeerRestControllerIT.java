26ce5c29ab6c33b07cd632f17486b8d54b9d3ec9
Andrei Balzan <andreibalzan@gmail.com>
2020-11-04 16:15:56 +0000

    creating a custom Authentication filter normally  used with legacy applications

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4")
+                .header("Api-Key", "spring").header("Api-Secret", "password"))
+                .andExpect(status().isOk());
+
+    }
 
     @Test
     void findBeer() throws Exception {
@@ -24,7 +31,6 @@ public class BeerRestControllerIT extends BaseIT {
                 .andExpect(status().isOk());
 
     }
-
 
     @Test
     void findBeersByUPC() throws Exception {
