5181b224afb8c1a0aeffe038f901568a8ea8d133
Andrei Balzan <andreibalzan@gmail.com>
2020-12-10 08:54:07 +0000

    removing Custom AuthFilters as they were used just as sample!

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -11,42 +11,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class BeerRestControllerIT extends BaseIT {
 
-
-    @Test
-    void deleteBeerUrl() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4")
-                .param("Api-Key", "spring").param("Api-Secret", "test"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void deleteBeerBadCredentialsUrl() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4")
-                .param("Api-Key", "spring").param("Api-Secret", "wrongPass"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void deleteBeerBadCredentials() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4")
-                .header("Api-Key", "spring").header("Api-Secret", "wrongPass"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void deleteBeer() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4")
-                .header("Api-Key", "spring").header("Api-Secret", "test"))
-                .andExpect(status().isOk());
-
-    }
-
     @Test
     void deleteBeerHttpBasic() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4")
                 .with(httpBasic("spring", "test")))
                 .andExpect(status().is2xxSuccessful());
-
     }
 
 
@@ -54,28 +23,24 @@ public class BeerRestControllerIT extends BaseIT {
     void deleteBeerNoAuth() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4"))
                 .andExpect(status().isUnauthorized());
-
     }
 
     @Test
     void findBeer() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer"))
                 .andExpect(status().isOk());
-
     }
 
     @Test
     void findBeersById() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/38dfa781-3290-4fb4-ba95-a8df3dcf1fb4"))
                 .andExpect(status().isOk());
-
     }
 
     @Test
     void findBeersByUPC() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beerUpc/234567890"))
                 .andExpect(status().isOk());
-
     }
 
 }
